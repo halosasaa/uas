79928e4472dd3fdd2ec6eb1322a7bf4ed83db75b
halosasaa <sasadwi912@gmail.com>
2023-07-24 14:25:05 +0700

    menu jenis menu done

--- resources/views/front/fjenismenu.blade.php
+++ resources/views/front/fjenismenu.blade.php
@@ -3,9 +3,9 @@
 @section('konten')
 <div class="container-fluid pt-5">
     <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span
-        class="bg-secondary pr-3">Jenis Menu</span></h2>
-    @foreach ($res_jenis_menu as $item)   
+        class="bg-secondary pr-3">Jenis Menu</span></h2>   
     <div class="row px-xl-5 pb-3">
+        @foreach ($res_jenis_menu as $item)
         <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
             <a class="text-decoration-none" href="">
                 <div class="cat-item d-flex align-items-center mb-4">
@@ -19,7 +19,7 @@
                 </div>
             </a>
         </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
